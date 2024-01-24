17abddcbd8e20dfda418fed211249aeab47a64b0
Utkarsh Ahuja <ahujautkarsh80@gmail.com>
2024-01-24 21:43:08 +0530

    feat: projects list updated

--- src/app/Components/Main/Projects/ProjectCard/index.tsx
+++ src/app/Components/Main/Projects/ProjectCard/index.tsx
@@ -15,14 +15,20 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ id, liveLink, githubLink, tit
         <div className="md:w-[30%] w-[90%] mx-auto h-[60%] relative md:my-5 my-9" key={id}>
             <Image className="rounded-xl z-10" src={`/projects/${imageSrc}`} alt={title} width={300} height={300} />
             <div className="flex text-[#FFDB70] items-center z-30 justify-center absolute rounded-xl inset-0 bg-black bg-opacity-50">
-                <a href={liveLink} target="_blank" rel="noopener noreferrer">
-                    <div className="bg-[#383838] p-2 rounded-lg mr-4"><FaEye size={23} /></div>
-                </a>
-                <a href={githubLink} target="_blank" rel="noopener noreferrer">
-                    <div className="bg-[#383838] p-2 rounded-lg"><IoLogoGithub size={23} /></div>
-                </a>
+                {
+                    liveLink!="null" &&
+                    <a href={liveLink} target="_blank" rel="noopener noreferrer">
+                        <div className="bg-[#383838] p-2 rounded-lg mr-4"><FaEye size={23} /></div>
+                    </a>
+                }
+                {
+                    githubLink!="null" &&
+                    <a href={githubLink} target="_blank" rel="noopener noreferrer">
+                        <div className="bg-[#383838] p-2 rounded-lg"><IoLogoGithub size={23} /></div>
+                    </a>
+                }
             </div>
-            <p className="text-[#E3E3E3] absolute pt-1 pl-[5%]">{title}</p>
+            <p className="text-[#E3E3E3] absolute pt-1 pl-[5%] tracking-wider">{title}</p>
         </div>
     )
 }
