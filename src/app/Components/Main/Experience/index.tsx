72625020ac7305c0cb536ef5a91b0791346c9f25
Utkarsh Ahuja <ahujautkarsh80@gmail.com>
2024-01-25 01:53:42 +0530

    feat: resume added

--- src/app/Components/Main/Experience/index.tsx
+++ src/app/Components/Main/Experience/index.tsx
@@ -1,8 +1,20 @@
-
+import { IoIosFolderOpen } from "react-icons/io";
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <div>
+      <div className="p-8">
+        <div className="w-[80%] md:w-[28%] md:ml-[72%]">
+          <a href="/Utkarsh_Ahuja.pdf" download>
+            <div className="flex text-[#FFDB70] shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] py-3 px-5 rounded-lg bg-gradient-to-r from-[#FFDB70]/30 to-[#202022]">
+              <IoIosFolderOpen size={20} />
+              <p className="pl-2">Download Resume</p>
+            </div>
+          </a>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
--- src/app/Components/Main/index.tsx
+++ src/app/Components/Main/index.tsx
@@ -8,7 +8,7 @@ import Contact from "./Contact";
 import Blogs from "./Blogs";
 
 const Main = () => {
-  const [selectedCard, setSelectedCard] = useState("Blogs");
+  const [selectedCard, setSelectedCard] = useState("Experience");
 
   return (
     <div>
