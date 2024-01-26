e16407e62ad0331affa6b1c7b5dfb3bfed8fa267
Utkarsh Ahuja <ahujautkarsh80@gmail.com>
2024-01-27 02:04:30 +0530

    fix: skills ui

--- src/app/Components/Main/Skills/index.tsx
+++ src/app/Components/Main/Skills/index.tsx
@@ -20,7 +20,7 @@ const Skills = () => {
     <div>
       <div className="w-[90%] text-[#FAFAFA] bg-[#232325] mx-auto my-[4vh] md:my-[9vh] rounded-md py-4 px-4 md:px-10 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] border-[#303030] border-[1px]">
         {/* Languages */}
-        <div className="flex flex-wrap justify-between my-5">
+        <div className="grid grid-cols-2 md:grid-cols-4 justify-start my-5">
           <div className="flex">
             <FaC size={30}/>
             <span className="ml-2 text-[1.3rem]">C</span>
@@ -41,7 +41,7 @@ const Skills = () => {
         <hr className="bg-[#383838] h-[0.1rem] w-[70%] mx-auto mt-[1vh] md:mt-[3vh] border-0"></hr>
         {/* Frameworks */}
         <div>
-        <div className="flex flex-wrap justify-between my-8">
+        <div className="grid grid-cols-2 md:grid-cols-4 justify-start my-8">
           <div className="flex">
             <SiNextdotjs size={30}/>
             <span className="ml-2 text-[1.3rem]">Next.JS</span>
@@ -58,20 +58,20 @@ const Skills = () => {
             <DiMongodb size={40}/>
             <span className="ml-2 text-[1rem] md:text-[1.3rem]">MongoDB</span>
           </div>
-          <div className="flex">
+          <div className="flex mt-2">
             <SiExpress size={40}/>
             <span className="ml-2 text-[1rem] md:text-[1.3rem]">Express</span>
+          </div>
+          <div className="flex mt-2">
+            <SiTailwindcss size={40}/>
+            <span className="ml-2 text-[1.3rem]">TailwindCSS</span>
           </div>
         </div>
         </div>
         <hr className="bg-[#383838] h-[0.1rem] w-[70%] mx-auto mt-[1vh] md:mt-[3vh] border-0"></hr>
         {/* Tools */}
         <div>
-        <div className="flex flex-wrap justify-between my-8">
-          <div className="flex">
-            <SiTailwindcss size={30}/>
-            <span className="ml-2 text-[1.3rem]">TailwindCSS</span>
-          </div>
+        <div className="grid grid-cols-2 md:grid-cols-4 justify-start my-8">
           <div className="flex">
             <IoLogoDocker size={40}/>
             <span className="ml-2 text-[1.3rem]">Docker</span>
--- src/app/Components/Main/index.tsx
+++ src/app/Components/Main/index.tsx
@@ -8,7 +8,7 @@ import Contact from "./Contact";
 import Blogs from "./Blogs";
 
 const Main = () => {
-  const [selectedCard, setSelectedCard] = useState("Contact");
+  const [selectedCard, setSelectedCard] = useState("Skills");
 
   return (
     <div>
